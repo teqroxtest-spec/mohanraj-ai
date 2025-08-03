50e1a66ae7a7eea8fbf34e6744d036699469951e
teqroxtest-spec <teqroxtest@gmail.com>
2025-08-03 13:12:25 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to LynqBot - Content to Conversion Platform</h1>
+    </div>
+  );
+};
+
+export default App;
